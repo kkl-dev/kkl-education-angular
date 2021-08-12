3f1c214cfaee85aabf755adb8287384bdd5d850b
dvir <dmenajem@gmail.com>
2021-08-12 14:34:43 +0300

    update info tab tooltip

--- src/app/screens/education-results/tooltip/tooltip.component.ts
+++ src/app/screens/education-results/tooltip/tooltip.component.ts
@@ -1,14 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+export interface TooltipDataModel {
+  hour: Number; avialable: boolean
+}
+
 @Component({
   selector: 'app-tooltip',
   templateUrl: './tooltip.component.html',
   styleUrls: ['./tooltip.component.scss'],
 })
+
+
 export class TooltipComponent implements OnInit {
-  @Input() hours: { hour: Number; avialable: boolean }[] = [];
+
+  @Input() hours: TooltipDataModel[];
   constructor() {}
 
   ngOnInit(): void {
+
   }
 }
