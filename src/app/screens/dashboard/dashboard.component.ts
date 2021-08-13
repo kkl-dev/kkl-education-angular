a301ad1be34042854b1ab8012218664198577768
DvirMon <dmenajem@gmail.com>
2021-08-13 15:25:42 +0300

    add global theme + sass map

--- src/app/screens/dashboard/dashboard.component.ts
+++ src/app/screens/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import { UserDataService } from './../../utilities/services/user-data.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
+
+const THUMBUP_ICON = `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.` +
+  `44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5` +
+  `1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/>
+  </svg>
+`;
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss'],
 })
+
 export class DashboardComponent implements OnInit {
   public activeRoute: string = '';
-  public dashboardArray:{
+  public dashboardArray: {
     svgUrl: string
     name: string
     url: string
@@ -19,10 +31,12 @@ export class DashboardComponent implements OnInit {
 
   constructor(
     private route: ActivatedRoute,
-    private userDataService: UserDataService
+    private userDataService: UserDataService,
+    private iconRegistry: MatIconRegistry,
+    private sanitizer: DomSanitizer
   ) {
     this.prefix = 'education';
-    this.dashboardArray =[
+    this.dashboardArray = [
       {
         svgUrl: 'assets/images/05-destination.svg',
         name: 'הטיולים שלי',
@@ -40,7 +54,9 @@ export class DashboardComponent implements OnInit {
       },
     ]
 
+    this.iconRegistry.addSvgIconLiteral('thumbs-up', sanitizer.bypassSecurityTrustHtml(THUMBUP_ICON));
+
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
