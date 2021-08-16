51cf794f2a7e91219d3c28b34d0f3654a2a38942
DvirMon <dmenajem@gmail.com>
2021-08-16 16:20:50 +0300

    fix conflocts

--- src/app/components/form-container/question-base.ts
+++ src/app/components/form-container/question-base.ts
@@ -1,45 +1,45 @@
 import { Validators } from '@angular/forms';
 export class QuestionBase<T> {
-  value: T | undefined | number;
-key: string;
-label: string;
-icon: string;
-disabled: boolean;
-order: number;
-controlType: string;
-columns: string;
-component: string;
-validations: Validators[];
-type: string;
-options: { key: string; value: string } [];
+  value: any;
+  key: string;
+  label: string;
+  icon: string;
+  disabled: boolean;
+  order: number;
+  controlType: string;
+  columns: string;
+  component: string;
+  validations: Validators[];
+  type: string;
+  options: { key: string; value: string }[];
 
-constructor(
-  options: {
-    value?: T;
-    key?: string;
-    icon?: string;
-    disabled?: string;
-    label?: string;
-    order?: number;
-    controlType?: string;
-    type?: string;
-    columns?: string;
-    component?: string;
-    validations?: Validators[];
-    options?: { key: string; value: string }[];
-  } = {}
-) {
-  this.value = options.value;
-  this.key = options.key || '';
-  this.icon = options.key || '';
-  this.disabled = true;
-  this.label = options.label || '';
-  this.columns = options.columns || '1';
-  this.validations = options.validations || [],
-    this.order = options.order === undefined ? 1 : options.order;
-  this.controlType = options.controlType || '';
-  this.type = options.type || '';
-  this.component = options.component || '';
-  this.options = options.options || [];
-}
+  constructor(
+    options: {
+      value?: any;
+      key?: string;
+      icon?: string;
+      disabled?: string;
+      label?: string;
+      order?: number;
+      controlType?: string;
+      type?: string;
+      columns?: string;
+      component?: string;
+      validations?: Validators[];
+      options?: { key: string; value: string }[];
+    } = {}
+  ) {
+    this.value = options.value;
+    this.key = options.key || '';
+    this.icon = options.key || '';
+    this.disabled = true;
+    this.label = options.label || '';
+    this.columns = options.columns || '1';
+    this.validations = options.validations || [],
+      this.order = options.order === undefined ? 1 : options.order;
+    this.controlType = options.controlType || '';
+    this.type = options.type || '';
+    this.component = options.component || '';
+    this.options = options.options || [];
+  }
 }
