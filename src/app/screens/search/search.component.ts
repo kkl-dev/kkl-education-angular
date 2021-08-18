1785a76f874626dd8e9ed1e7ca0208b6bc411cd7
dvir <dmenajem@gmail.com>
2021-08-18 11:02:08 +0300

    update search

--- src/app/screens/search/search.component.ts
+++ src/app/screens/search/search.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-search',
-  templateUrl: './search.component.html',
+  templateUrl: './search.component.html', 
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
