4bbcb46de370d79bc4b59f552e77a6803a146bfc
dvir <dmenajem@gmail.com>
2021-08-19 09:52:39 +0300

    add route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,20 +4,41 @@ import { DashboardComponent } from './screens/dashboard/dashboard.component';
 import { EducationResultsComponent } from './screens/education-results/education-results.component';
 import { LoginBackdropComponent } from './screens/login-backdrop/login-backdrop.component';
 import { MyToursComponent } from './screens/my-tours/my-tours.component';
+import { AdditionsComponent } from './screens/order-tour/additions/additions.component';
 import { OrderTourComponent } from './screens/order-tour/order-tour.component';
+import { SleepingOptionsComponent } from './screens/order-tour/sleeping-options/sleeping-options.component';
+import { SquadAssembleComponent } from './screens/order-tour/squad-assemble/squad-assemble.component';
 import { SearchComponent } from './screens/search/search.component';
 
-const routes: Routes = [  { path: 'login', component: LoginBackdropComponent },
-{ path: ':type', component: DashboardComponent },
-{ path: ':type/search', component: SearchComponent },
-{ path: 'education/results', component: EducationResultsComponent },
-{ path: 'education/my-tours', component: MyToursComponent },
-{ path: 'education/order-tour', component: OrderTourComponent },
-{ path: '**', component: LoginBackdropComponent, pathMatch: 'full' }
+const routes: Routes = [
+  { path: 'login', component: LoginBackdropComponent },
+  { path: ':type', component: DashboardComponent },
+  { path: ':type/search', component: SearchComponent },
+  { path: 'education/results', component: EducationResultsComponent },
+  {
+    path: 'education/my-tours',
+    component: MyToursComponent,
+    children: [
+      {
+        path: 'squad-assemble',
+        component: SquadAssembleComponent,
+      },
+      {
+        path: 'sleepings',
+        component: SleepingOptionsComponent,
+      },
+      {
+        path: 'additions',
+        component: AdditionsComponent,
+      },
+    ],
+  },
+  { path: 'education/order-tour', component: OrderTourComponent },
+  { path: '**', component: LoginBackdropComponent, pathMatch: 'full' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
