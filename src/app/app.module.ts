820af8c5b1252f5817310ed6015ca4c11a49f5ff
dvir <dmenajem@gmail.com>
2021-08-22 10:35:08 +0300

    update app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -56,13 +56,13 @@ import { FormQuestionComponent } from './components/form/form-question/form-ques
 import { NavCardComponent } from './components/nav-card/nav-card.component';
 import { FormInputComponent } from './components/form/form-input/form-input.component';
 import { InputRadioComponent } from './components/form/input-radio/input-radio.component';
-import { ExpendPanelComponent } from './components/expend-panel/expend-panel.component';
-import { CardTitleComponent } from './screens/additions/card-title/card-title.component';
 import { SleepingOptionsComponent } from './screens/order-tour/sleeping-options/sleeping-options.component';
 import { SleepingOptionsFormComponent } from './screens/order-tour/sleeping-options/sleeping-options-form/sleeping-options-form.component';
 import { SleepingOptionsByDayComponent } from './components/sleeping-options-by-day/sleeping-options-by-day.component';
 import { SquadAssembleComponent } from './screens/order-tour/squad-assemble/squad-assemble.component';
 import { NumberToTimePipe } from './utilities/pipes/numberToTime.pipe';
+import { ExpendPanelComponent } from './components/expend-panel/expend-panel.component';
+import { CardTitleComponent } from './screens/additions/card-title/card-title.component';
 import { TourPanelComponent } from './screens/order-tour/additions/tour-panel/tour-panel.component';
 import { TypographyComponent } from './components/typography/typography.component';
 
