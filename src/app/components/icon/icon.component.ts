fa98e5a44459892a05d756014a9ef1366b4acb22
dvir <dmenajem@gmail.com>
2021-08-23 14:12:58 +0300

    update navugation grid

--- src/app/components/icon/icon.component.ts
+++ src/app/components/icon/icon.component.ts
@@ -1,24 +1,27 @@
 import { IconsService } from './../../utilities/icons/icons.service';
 import { Component, OnInit, Input } from '@angular/core';
 
+export interface IconClasses {
+  default: boolean;
+  active: boolean;
+}
+
 @Component({
   selector: 'app-icon',
   templateUrl: './icon.component.html',
-  styleUrls: ['./icon.component.scss']
+  styleUrls: ['./icon.component.scss'],
 })
 export class IconComponent implements OnInit {
 
-  @Input() public type: string = "svg"
-  @Input() public isActive: boolean = false
-  @Input() public key: string = ""
-  @Input() public color: string = "primary"
+  @Input() public isActive: boolean;
+  @Input() public type: string = 'svg';
+  @Input() public key: string = '';
+  @Input() public color: string;
 
-  constructor(
-    private iconsService: IconsService
-  ) { }
+  constructor(private iconsService: IconsService) {}
 
   ngOnInit(): void {
-    this.iconsService.setIcon(this.key)
+    this.iconsService.setIcon(this.key);
+    console.log(this.isActive)
   }
-
 }
