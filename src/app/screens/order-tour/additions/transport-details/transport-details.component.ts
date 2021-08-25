6ee2728fd3126cb35147a36fb9ce80e50a2103a4
dvir <dmenajem@gmail.com>
2021-08-25 15:53:51 +0300

    update table cancelation

--- src/app/screens/order-tour/additions/transport-details/transport-details.component.ts
+++ src/app/screens/order-tour/additions/transport-details/transport-details.component.ts
@@ -3,6 +3,12 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { TableCellModel } from 'src/app/utilities/models/TableCell';
 import { columns, details, summery, supplier } from 'src/mock_data/additions';
 
+export interface TableData {
+columns : TableCellModel[],
+rows : TableCellModel[][],
+}
+
+
 @Component({
   selector: 'app-transport-details',
   templateUrl: './transport-details.component.html',
@@ -10,13 +16,15 @@ import { columns, details, summery, supplier } from 'src/mock_data/additions';
 })
 export class TransportDetailsComponent implements OnInit {
   public title: string = 'פרטים נוספים';
-  public cancelMode: boolean = false;
+  public editMode: boolean = false;
 
   public detailsColumns: TableCellModel[] = columns;
   public detailsTable = [details, supplier, summery];
 
-  public detailsSubject = new BehaviorSubject<TableCellModel[][]>(this.detailsTable);
-  public data$ = this.detailsSubject.asObservable()
+  public detailsSubject = new BehaviorSubject<TableCellModel[][]>(
+    this.detailsTable
+  );
+  public data$ = this.detailsSubject.asObservable();
 
   public cancelColumns: TableCellModel[] = [
     {
@@ -25,7 +33,7 @@ export class TransportDetailsComponent implements OnInit {
     },
   ];
 
-  private row: TableCellModel[] = [
+  private cancellationForm: TableCellModel[] = [
     {
       key: 'cancel',
       label: 'סיבת ביטול',
@@ -40,29 +48,52 @@ export class TransportDetailsComponent implements OnInit {
     },
   ];
 
-  public cancelTable: TableCellModel[][] = [this.row];
+  private cancellation: TableCellModel[] = [
+    {
+      key: 'date',
+      type: 'date',
+      label: 'בוטל בתאריך',
+      value : new Date()
+    },
+    {
+      key: 'concat',
+      label: 'בוטל ע"י',
+      value : 'גל שרון'
+    },
+    {
+      key: 'permission',
+      label: 'אישור ביטול ע"י',
+      value : 'קורנה'
+    },
+    {
+      key: 'contactId',
+      label: 'ת.ז.',
+      value : 4839849344
+    },
+    {
+      key: 'cancel',
+      label: 'סיבת ביטול',
+      value : 'קורנה',
+      offset: 3,
+    },
+  ];
+
+  public cancelTable: TableCellModel[][] = [this.cancellationForm];
 
   constructor() {}
 
-  ngOnInit(): void {}
-
-  public onClick() {
-    if (this.cancelMode) {
-      console.log('push');
-    }
-
-    this.detailsTable.push(summery);
-    this.detailsSubject.next(this.detailsTable)
-    // this.cancelMode = !this.cancelMode;
+  ngOnInit(): void {
+    this.openCancelForm()
   }
 
-  private updateCancelDetails() {
-    const cancel = {
-      key: 'cancel',
-      label: '',
-      offset: 3,
-    };
+  public openCancelForm() {
+    this.detailsTable.push(this.cancellationForm);
+    this.detailsSubject.next(this.detailsTable);
+    this.editMode = true;
+  }
 
-    this.row.push(cancel);
+  public updateCancelForm() {
+    this.detailsTable[(this.detailsTable.length - 1)] = this.cancellation;
+    this.detailsSubject.next(this.detailsTable);
   }
 }
