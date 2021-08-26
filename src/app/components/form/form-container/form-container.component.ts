f6bea9a33175be1bab229eff3df3656df5cd5baf
dvir <dmenajem@gmail.com>
2021-08-26 09:04:41 +0300

    feat - form-container : output form object

--- src/app/components/form/form-container/form-container.component.ts
+++ src/app/components/form/form-container/form-container.component.ts
@@ -18,6 +18,7 @@ import { QuestionBase } from '../logic/question-base';
 export class FormContainerComponent implements OnInit {
   public form!: FormGroup;
   @Output() formData: EventEmitter<any> = new EventEmitter();
+  @Output() formGroup: EventEmitter<FormGroup> = new EventEmitter();
 
   @Input() cols: string;
   @Input() gutter: string = '3';
@@ -43,6 +44,7 @@ export class FormContainerComponent implements OnInit {
   private subscribeToFormValues() {
     this.form.valueChanges.subscribe((values) => {
       this.formData.emit(values);
+      this.formGroup.emit(this.form)
     });
   }
 }
