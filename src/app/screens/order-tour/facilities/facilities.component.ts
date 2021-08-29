17ac2ff2f08d0851e87c1983c852f0a23f9219e3
Victor <victor@comrax.com>
2021-08-29 18:32:19 +0300

    working on timeline component

--- src/app/screens/order-tour/facilities/facilities.component.ts
+++ src/app/screens/order-tour/facilities/facilities.component.ts
@@ -15,13 +15,9 @@ export class FacilitiesComponent implements OnInit {
     this.fillTimes();
   }
 
-  fillTimes(): void {
+  public fillTimes(): void {
     for (let i = 0; i < 24; i++) {
-      if (i < 10) {
-        this.timesArray.push(`0${i}:00`);
-      } else {
-        this.timesArray.push(`${i}:00`);
-      }
+      i < 10 ? this.timesArray.push(`0${i}:00`) : this.timesArray.push(`${i}:00`);
     }
   }
 
