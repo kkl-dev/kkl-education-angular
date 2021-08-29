247791e5cfcf022c62e4b86eaead8006bf9f16ac
Victor <victor@comrax.com>
2021-08-29 15:01:17 +0300

    feature(added times array & function to fill array with times)

--- src/app/screens/order-tour/facilities/facilities.component.ts
+++ src/app/screens/order-tour/facilities/facilities.component.ts
@@ -5,11 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './facilities.component.html',
   styleUrls: ['./facilities.component.scss']
 })
+
 export class FacilitiesComponent implements OnInit {
+  public timesArray: Array<string | number> = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.fillTimes();
+  }
+
+  fillTimes(): void {
+    for (let i = 0; i < 24; i++) {
+      this.timesArray.push(i);
+    }
+    console.log(this.timesArray);
   }
 
 }
