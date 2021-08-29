ce0c1815d11f981c1d4250965dab14b4890139da
Victor <victor@comrax.com>
2021-08-29 15:21:32 +0300

    feature(added timeline)

--- src/app/screens/order-tour/facilities/facilities.component.ts
+++ src/app/screens/order-tour/facilities/facilities.component.ts
@@ -17,9 +17,12 @@ export class FacilitiesComponent implements OnInit {
 
   fillTimes(): void {
     for (let i = 0; i < 24; i++) {
-      this.timesArray.push(i);
+      if (i < 10) {
+        this.timesArray.push(`0${i}:00`);
+      } else {
+        this.timesArray.push(`${i}:00`);
+      }
     }
-    console.log(this.timesArray);
   }
 
 }
