83c16173ae7d7344eb2d434e69ec8a9ff52b9974
Victor <victor@comrax.com>
2021-08-30 18:21:11 +0300

    fix(try to build time line with table )

--- src/app/screens/order-tour/facilities/facilities.component.ts
+++ src/app/screens/order-tour/facilities/facilities.component.ts
@@ -26,13 +26,13 @@ export class FacilitiesComponent implements OnInit {
       title: 'ארוחת צהריים', startTime: '12:00', endTime: '13:00',
       iconSrc: 'assets/images/roast-chicken.svg', color: this.colors.green
     },
-    {
-      title: 'מטבח שדה', startTime: '12:00', endTime: '13:00',
-      iconSrc: 'assets/images/kitchen.svg', color: this.colors.blue
-    },
-    {
-      title: 'התייצבות', startTime: '10:00', endTime: '11:00',
-      iconSrc: 'assets/images/finish-flag-1.svg', color: this.colors.green ,secondIcon:'bus'
-    },
-  ]
+    // {
+    //   title: 'מטבח שדה', startTime: '12:00', endTime: '13:00',
+    //   iconSrc: 'assets/images/kitchen.svg', color: this.colors.blue
+    // },
+    // {
+    //   title: 'התייצבות', startTime: '10:00', endTime: '11:00',
+    //   iconSrc: 'assets/images/finish-flag-1.svg', color: this.colors.green ,secondIcon:'bus'
+    // },
+  ];
 }
