e81e85b4f48855009fa846a7bb5c187b4a17b203
dvir <dmenajem@gmail.com>
2021-09-01 11:57:42 +0300

    fix (form-header) - update colors

--- src/app/components/form/form-header/form-header.component.ts
+++ src/app/components/form/form-header/form-header.component.ts
@@ -1,21 +1,21 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-form-header',
   templateUrl: './form-header.component.html',
-  styleUrls: ['./form-header.component.scss']
+  styleUrls: ['./form-header.component.scss'],
 })
 export class FormHeaderComponent implements OnInit {
+  @Input() text: string;
+  @Input() custom: string;
+  @Input() title: string;
+  @Input() classes: {};
+  @Input() slots: {};
 
-
-  @Input() text : string
-  @Input() custom : string
-  @Input() classes : string[]
-  @Input() slots : {}
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.title = this.title || '';
+    console.log(this.classes);
   }
-
 }
